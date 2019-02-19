02cad5424ecde373b4a4ed6013248acbbe4b5b7b
sw-yx <swyx@dontemail.me>
2019-02-19 03:33:01 -1000

    makework

---
+++ static.config.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import path from 'path';
+
+export default {
+  plugins: ['react-static-plugin-typescript'],
+  entry: path.join(__dirname, 'src', 'index.tsx'),
+  devServer: {
+    proxy: {
+      // '/.netlify/functions': {
+      '/quiz': {
+        target: 'http://localhost:9000',
+        pathRewrite: {
+          '^/\\.netlify/functions/react-express-ssr': ''
+        }
+      }
+    }
+  },
+  getSiteData: () => ({
+    title: 'React Static'
+  })
+  // getRoutes: async () => {
+  //   const { data: posts } = await axios.get(
+  //     'https://jsonplaceholder.typicode.com/posts'
+  //   );
+  //   return [
+  //     {
+  //       path: '/blog',
+  //       getData: () => ({
+  //         posts
+  //       }),
+  //       children: posts.map(post => ({
+  //         path: `/post/${post.id}`,
+  //         component: 'src/containers/Post',
+  //         getData: () => ({
+  //           post
+  //         })
+  //       }))
+  //     }
+  //   ];
+  // }
+};
